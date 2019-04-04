7b027422555f453320221661b1e4f9674ac4890b
Adam Lind <sunboxnet@gmail.com>
2019-04-03 23:14:52 -0400

    Version 1.0.3. Update string helper getSingularWord.

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,6 +1,10 @@
 export function getSingularWord(pluralWord) {
   try {
-    return pluralWord.split(/s$/)[0].split(/ie$/)[0];
+    return (
+      pluralWord.slice(-3) === 'ies'
+      ? pluralWord.replace(/ies/, 'y')
+      : pluralWord.split(/s$/)[0].split(/ie$/)[0].split(/e$/)[0]
+    );
   } catch (error) {
     throw new Error('You must pass in a plural word!');
   }
